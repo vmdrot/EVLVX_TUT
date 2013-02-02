d20b3c4668687b6ba7c0ae80084740a58268c790
jefe <vmdrot@users.noreply.github.com>
2013-02-02 01:33:19 +0000

    + TV-19 - done

---
+++ JIRAAuthTest/JIRAAuthTest/JiraAuthenticatorProto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace JIRAAuthTest
+{
+    public class JiraAuthenticatorProto
+    {
+
+        protected string m_BaseUrl;
+        protected string m_Username;
+        protected string m_Password;
+
+        protected string RunQuery(
+            JiraResource resource,
+            string argument,
+            string data,
+            string method)
+        {
+            string url = string.Format("{0}{1}/", m_BaseUrl, resource.ToString());
+
+            if (argument != null)
+            {
+                url = string.Format("{0}{1}/", url, argument);
+            }
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.ContentType = "application/json";
+            request.Method = method;
+
+            if (data != null)
+            {
+                using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+                {
+                    writer.Write(data);
+                }
+            }
+
+            string base64Credentials = GetEncodedCredentials();
+            request.Headers.Add("Authorization", "Basic " + base64Credentials);
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            string result = string.Empty;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        private string GetEncodedCredentials()
+        {
+            string mergedCredentials = string.Format("{0}:{1}", m_Username, m_Password);
+            byte[] byteCredentials = UTF8Encoding.UTF8.GetBytes(mergedCredentials);
+            return Convert.ToBase64String(byteCredentials);
+        }
+    }
+    public enum JiraResource
+    {
+        project
+    }
+
+}
---
+++ JIRAAuthTest/JIRAAuthTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JIRAAuthTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Read();
+            String jiraRootUrl = args[0];
+            string usr = args[1];
+            string pwd = args[2];
+            string resp;
+            JiraAuthenticator auth = new JiraAuthenticator();
+            auth.JIRARootUrl = jiraRootUrl;
+            bool rslt = auth.Authenticate(usr, pwd, out resp);
+            Console.WriteLine("{0} ({1})\n{2}", rslt, auth.LastStatus, resp);
+        }
+    }
+}
