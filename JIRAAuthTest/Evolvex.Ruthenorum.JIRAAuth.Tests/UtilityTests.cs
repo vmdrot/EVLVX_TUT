5e43c115d3fa2f5709de475343efc96dc18a9c61
jefe <vmdrot@users.noreply.github.com>
2013-02-04 20:46:09 +0000

    +* TV-20 - in progress

---
+++ JIRAAuthTest/Evolvex.Ruthenorum.JIRAAuth.Tests/UtilityTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Evolvex.Web.Utility;
+using System.Net;
+
+namespace Evolvex.Ruthenorum.JIRAAuth.Tests
+{
+    [TestFixture]
+    public class UtilityTests
+    {
+
+        #region const's
+        private const string WEB_EXC_401_MSG = @"The remote server returned an error: (401) Unauthorized.";
+        private const string WEB_EXC_500_MSG = @"";
+        private const string WEB_EXC_403_MSG = @"The remote server returned an error: (403) Forbidden.";
+        private const string WEB_EXC_404_MSG = @"The remote server returned an error: (404) Not Found.";
+        
+        #endregion
+
+        [Test]
+        public void WebExceptionParserTest_401()
+        {
+            WebExceptionParserTestWorker(WEB_EXC_401_MSG);
+        }
+
+        [Test]
+        public void WebExceptionParserTest_403()
+        {
+            WebExceptionParserTestWorker(WEB_EXC_403_MSG);
+        }
+
+        [Test]
+        public void WebExceptionParserTest_404()
+        {
+            WebExceptionParserTestWorker(WEB_EXC_404_MSG);
+        }
+        
+        private void WebExceptionParserTestWorker(string excMsg)
+        {
+            HttpStatusCode? res = WebExceptionParser.Parse(excMsg);
+            Console.WriteLine("res = {0}", res);
+        }
+
+        [Test]
+        public void ObtainSampleWebExc401()
+        {
+            ObtainSampleWebExcWorker("http://pm.ruthenorum.info/jira/rest/api/2/user?username=valeriy.drotenko");
+        }
+
+        [Test]
+        public void ObtainSampleWebExc404()
+        {
+            ObtainSampleWebExcWorker("http://pm.ruthenorum.info/jira/nonexistentpage.html");
+        }
+
+        private void ObtainSampleWebExcWorker(String url)
+        { 
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    wc.DownloadString(url);
+                }
+                catch (WebException wexc)
+                {
+                    Console.WriteLine(wexc.Message);
+                }
+
+            }
+        }
+    }
+}
---
+++ JIRAAuthTest/Evolvex.Ruthenorum.JIRAAuth/Data/JIRAUserInfo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Evolvex.Ruthenorum.JIRAAuth.Data
+{
+    public class JIRAUserInfo
+    {
+        public string self { get; private set; }
+        public string name { get; private set; }
+        public string email { get; private set; }
+        public string displayName { get; private set; }
+        public bool active { get; private set; }
+        public string timeZone { get; private set; }
+        public List<string> groups { get; private set; }
+
+        private JIRAUserInfo(JsonTextReader reader)
+        {
+            string lastPropNm = string.Empty;
+            Dictionary<string, object> props = new Dictionary<string, object>();
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonToken.Boolean:
+                    case JsonToken.Bytes:
+                    case JsonToken.Date:
+                    case JsonToken.Float:
+                    case JsonToken.Integer:
+                    case JsonToken.Null:
+                    case JsonToken.String:
+                        props.Add(lastPropNm, reader.Value);
+                        break;
+                    case JsonToken.Comment:
+                        break;
+                    case JsonToken.EndArray:
+                        break;
+                    case JsonToken.EndConstructor:
+                        break;
+                    case JsonToken.EndObject:
+                        break;
+                    case JsonToken.None:
+                        break;
+                    case JsonToken.PropertyName:
+                        lastPropNm = reader.Value as string;
+                        break;
+                    case JsonToken.Raw:
+                        break;
+                    case JsonToken.StartArray:
+                        break;
+                    case JsonToken.StartConstructor:
+                        break;
+                    case JsonToken.StartObject:
+                        if (lastPropNm == "groups")
+                            props.Add(lastPropNm, ReadGroups(reader));
+                        else if (lastPropNm == "avatarUrls")
+                            props.Add(lastPropNm, ReadAvatars(reader));
+                        break;
+                    case JsonToken.Undefined:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private Dictionary<string, string> ReadAvatars(JsonTextReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> ReadGroups(JsonTextReader reader)
+        {
+            List<string> rslt = new List<string>();
+            while (reader.Read())
+            { 
+
+            }
+            return rslt;
+        }
+
+        public static JIRAUserInfo Parse(string json)
+        {
+            using(Newtonsoft.Json.JsonTextReader reader = new JsonTextReader(new StringReader(json)))
+            {
+                return new JIRAUserInfo(reader);
+            }
+
+        }
+    }
+}
