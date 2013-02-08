b74876d23c25b99cacbea6fa35bcd9bed187713c
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2013-02-08 14:39:15 +0000

    + TV-35 - php 2 po done (first version), doesn't work as expected

---
+++ PHP2PootleConverter/Evolvex.PHP2PootleConverter.Tests/PHP2PootleConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Evolvex.PHP2PootleConverter.Tests
+{
+    [TestFixture]
+    public class PHP2PootleConverterTests
+    {
+        [Test]
+        public void PHP2PootleTest()
+        {
+            Evolvex.PHP2PootleConverterLib.PHP2PootleConverter c = new Evolvex.PHP2PootleConverterLib.PHP2PootleConverter();
+            c.ProcessFile(@"D:\home\vmdrot\Testing\PHP2PootleConverter\attribute.php", Evolvex.PHP2PootleConverterLib.ConvertDirection.PHP2Pootle);
+        }
+    }
+}
