ef886964c288c87587bae9e973f6acc98ce4e992
jefe <vmdrot@users.noreply.github.com>
2013-02-08 13:51:16 +0000

    + TV-20 - in progress ..

--- JIRAAuthTest/Evolvex.Ruthenorum.JIRAAuth/Data/JIRAUserInfo.cs
+++ JIRAAuthTest/Evolvex.Ruthenorum.JIRAAuth/Data/JIRAUserInfo.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using System.IO;
+using System.Web.Security;
 
 namespace Evolvex.Ruthenorum.JIRAAuth.Data
 {
@@ -16,11 +17,13 @@ namespace Evolvex.Ruthenorum.JIRAAuth.Data
         public bool active { get; private set; }
         public string timeZone { get; private set; }
         public List<string> groups { get; private set; }
+        public Dictionary<String, String> avatarUrls { get; private set; }
 
         private JIRAUserInfo(JsonTextReader reader)
         {
             string lastPropNm = string.Empty;
             Dictionary<string, object> props = new Dictionary<string, object>();
+            #region read-out json
             while (reader.Read())
             {
                 switch (reader.TokenType)
@@ -65,20 +68,82 @@ namespace Evolvex.Ruthenorum.JIRAAuth.Data
                         break;
                 }
             }
+            #endregion
+
+            #region fill the object props
+            foreach (string prop in props.Keys)
+            {
+                switch (prop)
+                { 
+                    case "self":
+                        this.self = props[prop] as string;
+                        break;
+                    case "name":
+                        this.name = props[prop] as string;
+                        break;
+                    case "emailAddress":
+                        this.email = props[prop] as string;
+                        break;
+                    case "displayName":
+                        this.displayName = props[prop] as string;
+                        break;
+                    case "active":
+                        this.active = (bool)props[prop];
+                        break;
+                    case "timeZone":
+                        this.timeZone = props[prop] as string;
+                        break;
+                    case "groups":
+                        this.groups = (List<String>)props[prop];
+                        break;
+                    case "avatarUrls":
+                        this.avatarUrls = (Dictionary<string,string>)props[prop];
+                        break;
+                    default:
+                        break;
+                }
+            }
+            #endregion
         }
 
         private Dictionary<string, string> ReadAvatars(JsonTextReader reader)
         {
-            throw new NotImplementedException();
+            Dictionary<string, string> rslt = new Dictionary<string, string>();
+            bool arrayStarted = false;
+            string lastPropName = string.Empty;
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.EndObject)
+                    break;
+                if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    lastPropName = reader.Value as string;
+                }
+                if (reader.TokenType == JsonToken.String)
+                    rslt.Add(lastPropName, reader.Value as string);
+            }
+            return rslt;
         }
 
         private static List<string> ReadGroups(JsonTextReader reader)
         {
             List<string> rslt = new List<string>();
+            bool arrayStarted = false;
+            string lastPropName = string.Empty;
             while (reader.Read())
             {
-                if (reader.TokenType == JsonToken.EndObject)
+                if (reader.TokenType == JsonToken.StartArray)
+                    arrayStarted = true;
+                if (!arrayStarted)
+                    continue;
+                if (reader.TokenType == JsonToken.EndArray)
                     break;
+                if(reader.TokenType == JsonToken.PropertyName)
+                {
+                    lastPropName = reader.Value as string;
+                }
+                if (reader.TokenType == JsonToken.String && lastPropName == "name")
+                    rslt.Add(reader.Value as string);
             }
             return rslt;
         }
@@ -91,5 +156,16 @@ namespace Evolvex.Ruthenorum.JIRAAuth.Data
             }
 
         }
+
+        public override string ToString()
+        {
+            return string.Format("self = {0}, name = {1}, email = {2}, displayName = {3}, active = {4}, timeZone = {5}, groups.Count = {6}, avatarUrls.Count = {7}", self, name, email, displayName, active, timeZone, groups.Count, avatarUrls.Count);
+        }
+
+        public static explicit operator MembershipUser(JIRAUserInfo src)
+        {
+            return null; //todo
+        }
+
     }
 }
