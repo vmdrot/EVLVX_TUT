f42265efba5efadfb2f6ce0e475663d0b826dca6
jefe <vmdrot@users.noreply.github.com>
2013-02-08 14:39:15 +0000

    + TV-35 - php 2 po done (first version), doesn't work as expected

--- PHP2PootleConverter/Evolvex.PHP2PootleConverterLib/PHP2PootleConverter.cs
+++ PHP2PootleConverter/Evolvex.PHP2PootleConverterLib/PHP2PootleConverter.cs
@@ -14,13 +14,16 @@ namespace Evolvex.PHP2PootleConverterLib
 #. $1 $2\nmsgid ""$2""\nmsgstr ""// $2""";
         private const string RGX_TOKEN_PO = @"\$_\['([\w]+)'\]\s+=\s+'(.+)'
 #. $_['$1']\nmsgid ""$2""\nmsgstr """;
+        private const string PHP_HEADER_TOKEN = "<?php";
+        private const string PHP_TRAIL_TOKEN = "?>";
+
 
         public bool ProcessFile(string path, ConvertDirection dir)
         {
             return dir == ConvertDirection.PHP2Pootle ? PHP2Pootle(path) : Pootle2PHP(path);
         }
 
-        private bool PHP2Pootle(string path)
+        private bool PHP2Pootle_old(string path)
         {
             String txt = File.ReadAllText(path);
             Regex rHdr = new Regex(RGX_HEADER);
@@ -29,7 +32,21 @@ namespace Evolvex.PHP2PootleConverterLib
             trimmed = trimmed.Substring(0, trimmed.Length - 2);
             Regex rxFind = new Regex(RGX_TOKEN_PHP);
             Regex rxRepl = new Regex(RGX_TOKEN_PO);
-            rxFind.Replace(trimmed);
+            //rxFind.Replace(trimmed);
+            return false;
+        }
+
+        private bool PHP2Pootle(string path)
+        {
+            String txt = File.ReadAllText(path);
+            Regex rHdr = new Regex(RGX_HEADER);
+            Match m = rHdr.Match(txt);
+            string openingPhp = m.Groups[0].Captures[0].Value;
+            //string closingPhp = m.Groups[1].Captures[0].Value;
+            string trimmed = txt.Replace(openingPhp,"").Replace(/*closingPhp*/ PHP_TRAIL_TOKEN, "");
+            String rslt = Regex.Replace(trimmed, RGX_TOKEN_PHP, RGX_TOKEN_PO);
+            File.WriteAllText(path, rslt);
+            return true;
         }
 
 
