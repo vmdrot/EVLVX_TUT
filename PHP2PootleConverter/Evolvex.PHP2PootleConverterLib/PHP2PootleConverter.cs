f7e7de068850c8e55faf5a4fe91e21c8db9ebb70
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2013-02-18 00:05:43 +0000

    +* TV-35: fully turn-around-able

--- PHP2PootleConverter/Evolvex.PHP2PootleConverterLib/PHP2PootleConverter.cs
+++ PHP2PootleConverter/Evolvex.PHP2PootleConverterLib/PHP2PootleConverter.cs
@@ -4,55 +4,44 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
+using Evolvex.PHP2PootleConverterLib.Readers;
+using Evolvex.PHP2PootleConverterLib.Writers;
 
 namespace Evolvex.PHP2PootleConverterLib
 {
     public class PHP2PootleConverter
     {
         private const string RGX_HEADER = @"<\?php\s+|\s+\?>";
-        private const string RGX_TOKEN_PHP = @"(\/\/) ([a-zA-Z]+)
-#. $1 $2\nmsgid ""$2""\nmsgstr ""// $2""";
-        private const string RGX_TOKEN_PO = @"\$_\['([\w]+)'\]\s+=\s+'(.+)'
-#. $_['$1']\nmsgid ""$2""\nmsgstr """;
         private const string PHP_HEADER_TOKEN = "<?php";
         private const string PHP_TRAIL_TOKEN = "?>";
 
 
-        public bool ProcessFile(string path, ConvertDirection dir)
+        public bool ProcessFile(string srcPath, string targetPath, ConvertDirection dir)
         {
-            return dir == ConvertDirection.PHP2Pootle ? PHP2Pootle(path) : Pootle2PHP(path);
+            return dir == ConvertDirection.PHP2Pootle ? PHP2Pootle(srcPath, targetPath) : Pootle2PHP(srcPath, targetPath);
         }
 
-        private bool PHP2Pootle_old(string path)
+        private bool PHP2Pootle(string srcPath, string targetPath)
         {
-            String txt = File.ReadAllText(path);
-            Regex rHdr = new Regex(RGX_HEADER);
-            string pureTxt = rHdr.Match(txt).Groups[0].Captures[0].Value;
-            string trimmed = pureTxt.Substring(6);
-            trimmed = trimmed.Substring(0, trimmed.Length - 2);
-            Regex rxFind = new Regex(RGX_TOKEN_PHP);
-            Regex rxRepl = new Regex(RGX_TOKEN_PO);
-            //rxFind.Replace(trimmed);
-            return false;
-        }
-
-        private bool PHP2Pootle(string path)
-        {
-            String txt = File.ReadAllText(path);
-            Regex rHdr = new Regex(RGX_HEADER);
-            Match m = rHdr.Match(txt);
-            string openingPhp = m.Groups[0].Captures[0].Value;
-            //string closingPhp = m.Groups[1].Captures[0].Value;
-            string trimmed = txt.Replace(openingPhp,"").Replace(/*closingPhp*/ PHP_TRAIL_TOKEN, "");
-            String rslt = Regex.Replace(trimmed, RGX_TOKEN_PHP, RGX_TOKEN_PO);
-            File.WriteAllText(path, rslt);
+            PHPReader r = new PHPReader();
+            POWriter w = new POWriter();
+            StringBuilder sb = new StringBuilder();
+            w.WriteAll(r.ReadAll(File.ReadAllLines(srcPath)), sb);
+            File.WriteAllText(targetPath, sb.ToString());
             return true;
         }
 
 
-        private bool Pootle2PHP(string path)
+        private bool Pootle2PHP(string srcPath, string targetPath)
         {
-            return false;
+            POReader r = new POReader();
+            PHPWriter w = new PHPWriter();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(PHP_HEADER_TOKEN);
+            w.WriteAll(r.ReadAll(File.ReadAllLines(srcPath)), sb);
+            sb.AppendLine(PHP_TRAIL_TOKEN);
+            File.WriteAllText(targetPath, sb.ToString());
+            return true;
         }
 
     }
