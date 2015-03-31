048cf50db4cf02c943ad7d3443eae67f5b84b81d
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-03-31 12:11:32 +0000

    + BGU-19 Попроставляти Description та DisplayName для усіх пропертей - cont'd

--- BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Questionnaires/Appx2OwnershipStructLP.cs
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Questionnaires/Appx2OwnershipStructLP.cs
@@ -34,6 +34,7 @@ namespace BGU.DRPL.SignificantOwnership.Core.Questionnaires
         /// <summary>
         /// p.1.1 , p.1.2 , p.1.3 , p.1.6
         /// </summary>
+        [DisplayName("Юр.особа-заявник")]
         [Description("1. Інформація про юридичну особу")]
         public LegalPersonInfo Acquiree { get; set; }
 
@@ -41,9 +42,11 @@ namespace BGU.DRPL.SignificantOwnership.Core.Questionnaires
         /// p.1.4
         /// </summary>
         [Browsable(true)]
+        [DisplayName("Наглядова (спостережна) рада юридичної особи?")]
         [Description("1.4. Чи існує наглядова (спостережна) рада юридичної особи")]
         public bool IsSupervisoryCouncilPresent { get; set; }
         [Browsable(true)]
+        [DisplayName("Особовий склад наглядової(спостережної)ради юрособи")]
         [Description("1.4. Голова та члени наглядової (спостережної) ради юридичної особи")]
         public CouncilBodyInfo SupervisoryCouncil { get; set; }
         
@@ -51,21 +54,25 @@ namespace BGU.DRPL.SignificantOwnership.Core.Questionnaires
         /// p.1.5
         /// </summary>
         [Browsable(true)]
+        [DisplayName("Виконавчий орган юридичної особи?")]
         [Description("1.4. Чи існує виконавчий орган юридичної особи")]
         public bool IsExecutivesPresent { get; set; }
         [Browsable(true)]
+        [DisplayName("Особовий склад виконавчого органу юрособи")]
         [Description("1.5. Голова та члени виконавчого органу юридичної особи")]
         public CouncilBodyInfo Executives { get; set; }
         
         /// <summary>
         /// p.1.7
         /// </summary>
+        [DisplayName("% власності юрособи у капіталі банку")]
         [Description("1.7. Відсоток участі в банку становить ... відсотків статутного капіталу банку")]
         public decimal TotalOwneshipPct { get; set; }
         
         /// <summary>
         /// 
         /// </summary>
+        [DisplayName("Частка власності юрособи в банку - розподіл")]
         [Description("1.7. Відсоток участі в банку становить, у т.ч.:")]
         public TotalOwnershipDetailsInfo TotalOwneshipDetails { get; set; } //todo - how many rows
 
@@ -73,12 +80,14 @@ namespace BGU.DRPL.SignificantOwnership.Core.Questionnaires
         /// p.1.8
         /// </summary>
         //public List<CommonOwnershipInfo> BankExistingCommonImplicitOwners { get; set; }
+        [DisplayName("Розшифровка ланцюжка власників банку")]
         [Description("1.8. Інформація про спільне володіння (=розкриття усього ланцюжка власників, у т.ч. й пов'язаних юросіб)")]
         public List<OwnershipStructure> BankExistingCommonImplicitOwners { get; set; }
 
         /// <summary>
         /// p.1.10
         /// </summary>
+        [DisplayName("Частка власності, що набувається")]
         [Description("1.10. Інформація про набуття права голосу")]
         public List<PurchasedVoteInfo> SharesAppliedFor {get;set;}
 
@@ -87,15 +96,20 @@ namespace BGU.DRPL.SignificantOwnership.Core.Questionnaires
         /// <summary>
         /// p.2.1
         /// </summary>
-        [Description("2.1. Інформація про фізичних осіб, які володіють істотною участю в юридичній особі\n(якщо не було розкрито у п.1.8.)")]
+        [DisplayName("Розшифровка ланцюжка власників юрособи-набувача")]
+        [Description("2.1. Інформація про осіб (юр. і фіз.), які володіють істотною участю в юридичній особі\n(якщо не було розкрито у п.1.8.)")]
         public List<OwnershipStructure> ApplicantOwnershipStructure { get; set; }
 
+        [DisplayName("Реквізити осіб-фігурантів анкети")]
         [Description("Повна інформація про осіб, що згадуються в анкеті")]
         public List<GenericPersonInfo> MentionedIdentities { get; set; }
+        [DisplayName("Зв'язки між особами-фігурантами анкети")]
         [Description("Відомості про пов'язаних осіб, що згадуються в анкеті")]
         public List<PersonsAssociation> PersonsLinks { get; set; }
+        [DisplayName("Підписант")]
         [Description("Відомості по особу, що підписала анкету")]
         public SignatoryInfo Signatory { get; set; }
+        [DisplayName("Контакти")]
         [Description("Контактні дані відправника анкети")]
         public ContactInfo ContactPerson { get; set; }
 
