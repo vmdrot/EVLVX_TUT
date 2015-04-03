08acd98d4159f412a5fe16cb4134e3e8dd59ae93
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-04-03 17:12:36 +0000

    + BGU-41 UI - research devexpress capabilities - started

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.DXUILib/Forms/SimpleObjectFormDX.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace BGU.DRPL.SignificantOwnership.DXUILib.Forms
+{
+    public partial class SimpleObjectFormDX : DevExpress.XtraEditors.XtraForm
+    {
+        public SimpleObjectFormDX()
+        {
+            InitializeComponent();
+        }
+
+
+        public object DataSource
+        {
+            get { return propGrid.SelectedObject; }
+            set { propGrid.SelectedObject = value; }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SimpleObjectFormDX_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
