44c9e7497594c9d9cea93b8e58dd20fce00ef5e7
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-04-09 15:19:27 +0000

    + BGU-41 UI - research devexpress capabilities - in progress

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.DXUILib/TreeViewFormDX.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BGU.DRPL.SignificantOwnership.DXUILib
+{
+    partial class TreeViewFormDX
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeList = new DevExpress.XtraTreeList.TreeList();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeList
+            // 
+            this.treeList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeList.Location = new System.Drawing.Point(1, 1);
+            this.treeList.Name = "treeList";
+            this.treeList.Size = new System.Drawing.Size(838, 488);
+            this.treeList.TabIndex = 0;
+            // 
+            // TreeViewFormDX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 530);
+            this.Controls.Add(this.treeList);
+            this.Name = "TreeViewFormDX";
+            this.Text = "TreeViewFormDX";
+            ((System.ComponentModel.ISupportInitialize)(this.treeList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraTreeList.TreeList treeList;
+    }
+}
---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.DXUILib/TreeViewFormDX.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace BGU.DRPL.SignificantOwnership.DXUILib
+{
+    public partial class TreeViewFormDX : DevExpress.XtraEditors.XtraForm
+    {
+        public TreeViewFormDX()
+        {
+            InitializeComponent();
+        }
+    }
+}
