410f05c5ab68ec92d38a0aba7291feea5a03fa17
jefe <vmdrot@users.noreply.github.com>
2015-04-17 06:01:22 +0000

    + BGU-49 Довідник банків v.2.0 - proto, started

---
+++ BGU.DRPL.SignificantOwnership/BGU.Web20.MiscItemsSite/Controls/InstitutionSerchCriteria.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="InstitutionSerchCriteria.ascx.cs" Inherits="BGU.Web20.MiscItemsSite.Controls.InstitutionSerchCriteria" %>
+<table width="100%" class="scContainerTbl">
+ <tr>
+   <td><asp:Literal ID="lit" runat="server"></asp:Literal></td>
+   <td></td>
+ </tr>
+</table>
---
+++ BGU.DRPL.SignificantOwnership/BGU.Web20.MiscItemsSite/Facade/Enums.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.Web20.MiscItemsSite
+{
+    [Flags]
+    public enum InstitutionType
+    {
+        None = 0,
+        Bank,
+        NonBankFinancial
+    }
+
+    [Flags]
+    public enum InstitutionStatus
+    {
+        None = 0,
+        Operational,
+        Problem,
+        Insolvent,
+        CrisisAdministration,
+        UnderLiquidation,
+        Liquidated
+    }
+
+    public enum InstitutionLevel
+    {
+        None = 0,
+        TopLevel,
+        Branch,
+        Outlet,
+        ATM,
+        POS
+    }
+}
