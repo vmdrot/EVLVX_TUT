dcb7befff113bac078556d4a9282041707dda660
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-04-17 16:09:58 +0000

    + BGU-59 Схеми валідації - продумати спосіб документування : stub

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/ValidationSchemes/ValidationShemeBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.Core.ValidationSchemes
+{
+    public class ValidationShemeBaseAttribute : Attribute
+    {
+
+        public string condition;
+        public ValidationShemeBaseAttribute() { }
+        public ValidationShemeBaseAttribute(string condition) { this.condition = condition; }
+    }
+}
