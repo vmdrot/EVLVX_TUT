5935a36d7e1c373c08ff2158572955dee73e71d8
jefe <vmdrot@users.noreply.github.com>
2015-04-20 10:03:50 +0000

    +* BGU-58 Web UI, prototyping: research Bootstrap / materialize : still in progress

---
+++ BGU.DRPL.SignificantOwnership/BGU.Web20.MiscItemsSite/Facade/DataModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using BGU.DRPL.SignificantOwnership.Utility;
+
+namespace BGU.Web20.MiscItemsSite.Facade
+{
+    public class DataModule
+    {
+        private static DataTable _RcuKru;
+        public static DataTable RcuKru
+        {
+            get
+            {
+                if(_RcuKru == null)
+                {
+                    string rcuKruPath = ConfigurationManager.AppSettings.Get("rcuKruPath");
+                    _RcuKru = RcuKruReader.Read(rcuKruPath);
+                }
+                return _RcuKru;
+            }
+        }
+
+    }
+}
