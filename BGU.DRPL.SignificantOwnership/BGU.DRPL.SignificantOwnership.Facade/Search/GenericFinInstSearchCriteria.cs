f5216250f848e6d3bbaed28030d7af2a35bb87db
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-04-27 16:37:10 +0000

    +* BGU-72 Публікація ліцензій на веб-сайті: прототип : in progress +* BGU-73 Пошук фінансових установ 4 web2.0 : started

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Facade/Search/GenericFinInstSearchCriteria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BGU.DRPL.SignificantOwnership.Core.Spares2;
+using Evolvex.Utility.Core.Geo;
+
+namespace BGU.DRPL.SignificantOwnership.Facade.Search
+{
+    public class GenericFinInstSearchCriteria : SearchCriteriaBase
+    {
+        public string InstitutionSearchText { get; set; }
+        public string InstitutionTaxCode { get; set; }
+        public FinancialInstitutionType InstType { get; set; }
+        public FinancialInstitutionStatus InstStatus { get; set; }
+        public CompanyOwnershipType OwnershipType { get; set; }
+        public string ActivitiesSearchText { get; set; }
+        public BasicGeoposition NearPoint { get; set; }
+        public double? InRangeKm { get; set; }
+    }
+}
---
+++ BGU.DRPL.SignificantOwnership/Evolvex.Utility.Core/Geo/BasicGeoposition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.Utility.Core.Geo
+{
+    public class BasicGeoposition
+    {
+        public BasicGeoposition(): this(0, 0, 0)
+        {
+        }
+
+        public BasicGeoposition(double lat, double longt) :this (0, lat, longt)
+        { }
+
+        public BasicGeoposition(double alt, double lat, double longt)
+        {
+            Altitude = alt;
+            Latitude = lat;
+            Longitude = longt;
+        }
+
+        public double Altitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
