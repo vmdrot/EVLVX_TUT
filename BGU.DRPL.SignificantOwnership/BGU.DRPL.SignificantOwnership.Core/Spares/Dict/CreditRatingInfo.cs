14babf437cf1e1c65e9aa9da35265eeb6290155f
jefe <vmdrot@users.noreply.github.com>
2015-05-07 16:39:51 +0000

    +* BGU-21 RegNLic: АНКЕТА членів виконавчого органу та наглядової (Додаток 3) : almost DONE +* BGU-23 RegNLic: АНКЕТА юридичної особи (у тому числі банку) (Додаток 2) : in progress

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Spares/Dict/CreditRatingInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Evolvex.Utility.Core.ComponentModelEx;
+using System.ComponentModel;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares.Dict
+{
+    /// <summary>
+    /// Кредитний рейтинг емітента
+    /// Три поля, по суті:
+    /// 1) Аґенція, що роздала рейтинг;
+    /// 2) Довго- та середньотерміновий рейтинг;
+    /// 3) Короткотерміновий рейтинг.
+    /// Якщо аґенція - добре відома, то достатньо буде обрати зі списків WellKnownAgency, LongMidTermRatingValue і ShortTermRatingValue 
+    /// інакше, доведеться заповнити поле AgencyOther; якщо у полях значення рейтингу буде обрано "Інше" (Other), то також доведеться ввести значення у відповідному полі *RatingValueOther.
+    /// </summary>
+    public class CreditRatingInfo
+    {
+        [Required]
+        [DisplayName("Рейтингова аґенція (добре знана)")]
+        public WellKnownCreditRatingAgencyType WellKnownAgency { get; set; }
+        /// <summary>
+        /// Якщо в полі WellKnownAgency обрано "Інше" (Other)
+        /// </summary>
+        [DisplayName("Рейтингова аґенція (інша)")]
+        public RatingAgencyInfo AgencyOther { get; set; }
+        [Required]
+        [DisplayName("Довго- та середньотермінове значення рейтингу(стандартне)")]
+        public LongTermCreditRatingType LongMidTermRatingValue { get; set; }
+        /// <summary>
+        /// Якщо в полі LongMidTermRatingValue обрано "Інше" (Other)
+        /// </summary>
+        [DisplayName("Довго- та середньотермінове значення рейтингу(якщо інше)")]
+        public string LongMidTermRatingValueOther { get; set; }
+        [Required]
+        [DisplayName("Короткотермінове значення рейтингу(стандартне)")]
+        public ShortTermCreditRatingType ShortTermRatingValue { get; set; }
+        /// <summary>
+        /// Якщо в полі ShortTermRatingValue обрано "Інше" (Other)
+        /// </summary>
+        [DisplayName("Короткотермінове значення рейтингу(якщо інше)")]
+        public string ShortTermRatingValueOther { get; set; }
+    }
+}
---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Spares/Dict/EconomicActivityType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Evolvex.Utility.Core.ComponentModelEx;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares.Dict
+{
+    
+    public class EconomicActivityType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <seealso cref="http://kved.ukrstat.gov.ua/cgi-bin/kv-vi.exe?r=026492&l=%F4%E0%EA%F2%EE%F0%E8%ED%E3%EE%E2%E8%F5" />
+        [DisplayName("Код діяльності за КВЕД")]
+        [Required]
+        public string KVEDCode { get; set; }
+        [DisplayName("Назва діяльності за КВЕД")]
+        [Required]
+        public string KVEDActivityName { get; set; }
+        [DisplayName("Власна фактична назва діяльності")]
+        public string ActivitySelfName { get; set; }
+    }
+}
