f4c3f61b9db9b24f727f21910d6d7a52537af5ba
jefe <vmdrot@users.noreply.github.com>
2015-06-02 08:20:55 +0000

    + BGU-23 RegNLic: АНКЕТА юридичної особи (у тому числі банку) (Додаток 2) : лист про походження коштів

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Messages/RegLicAppx2OwnershipAcqRequestLP_MoneyOriginsLtr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Evolvex.Utility.Core.ComponentModelEx;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Messages
+{
+    public class RegLicAppx2OwnershipAcqRequestLP_MoneyOriginsLtr
+    {
+        /// <summary>
+        /// Стверджую, що юридична особа має можливість підтвердити походження джерел коштів, за рахунок яких придбає акції банку.
+        /// </summary>
+        [DisplayName("Може підтвердити походження джерел коштів?")]
+        [Description("Стверджую, що юридична особа має можливість підтвердити походження джерел коштів, за рахунок яких придбає акції банку.")]
+        [Required]
+        public bool CanProveMoneyOrigins { get; set; }
+    }
+}
