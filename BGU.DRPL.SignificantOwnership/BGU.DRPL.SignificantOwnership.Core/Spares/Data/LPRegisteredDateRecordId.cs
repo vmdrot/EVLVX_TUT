1617403afa3d2a0ea75587c711930236fd0c9032
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-02 08:19:36 +0000

    + BGU-104 Постанова 315: Інформація про пов'язаних осіб (XML) : started

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Spares/Data/LPRegisteredDateRecordId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
+{
+    /// <summary>
+    /// Дата та номер запису про проведення державної реєстрації фізичної особи-підприємця
+    /// </summary>
+    public class LPRegisteredDateRecordId
+    {
+        /// <summary>
+        /// Дата 
+        /// </summary>
+        [DisplayName("Дата запису")]
+        [Description("Дата запису про проведення державної реєстрації фізичної особи-підприємця")]
+        public DateTime RegisteredDate { get; set; }
+
+        /// <summary>
+        /// № запису
+        /// </summary>
+        [DisplayName("Номер запису")]
+        [Description("Номер запису про проведення державної реєстрації фізичної особи-підприємця")]
+        public string RegistryRecordId { get; set; }
+
+    }
+}
