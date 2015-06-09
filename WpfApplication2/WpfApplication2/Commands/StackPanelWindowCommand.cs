5fa08004371c095531a003fe4530b302caedeeb5
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-09 16:43:14 +0000

    + BGU-105 WPF XAML генератор (зі структур) : proto

---
+++ WpfApplication2/WpfApplication2/Commands/StackPanelWindowCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication2.Commands
+{
+    public class StackPanelWindowCommand : System.Windows.Input.ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            //e.CanExecute = true;
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            StackPanelWindow wnd = new StackPanelWindow();
+            wnd.ShowDialog();
+        }
+    }
+}
---
+++ WpfApplication2/WpfApplication2/Controls/SimpleDropDownControl2.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для SimpleDropDownControl2.xaml
+    /// </summary>
+    public partial class SimpleDropDownControl2 : UserControl
+    {
+        public SimpleDropDownControl2()
+        {
+            InitializeComponent();
+        }
+
+        [Browsable(true)]
+        public string DisplayName
+        {
+            get { return lblDisplayName.Content as string; }
+            set { lblDisplayName.Content = value; }
+        }
+
+        [Browsable(true)]
+        public string Description 
+        {
+            get { return lblDescription.Content as string; }
+            set { lblDescription.Content = value; }
+        }
+
+        public IEnumerable ListSource 
+        {
+            get { return cbx.ItemsSource; }
+            set { cbx.ItemsSource = value; }
+        }
+
+        [Browsable(true)]
+        public String DisplayMember { get { return cbx.DisplayMemberPath; } set { cbx.DisplayMemberPath = value; } }
+        
+        [Browsable(true)]
+        public String ValueMember { get { return cbx.SelectedValuePath; } set { cbx.SelectedValuePath = value; } }
+
+        public object SelectedValue 
+        {
+            get { return cbx.SelectedValue; }
+            set { cbx.SelectedValue = value; }
+        }
+    }
+}
