5205efff3076d8548dad067d5a3ce88496a14aca
jefe <vmdrot@users.noreply.github.com>
2015-06-12 17:16:37 +0000

    + BGU-23 RegNLic: АНКЕТА юридичної особи (у тому числі банку) (Додаток 2) : лист про походження коштів

---
+++ BGU.DRPL.SignificantOwnership/BGU.DRPL.SignificantOwnership.Core/Spares/Data/IPOSharesPurchaseInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
+{
+    public class IPOSharesPurchaseInfo
+    {
+        public int SharesCount { get; set; }
+        public CurrencyAmount NominalSharePrice { get; set; }
+        public CurrencyAmount NominalTotalSharesValue { get; set; }
+        public CurrencyAmount ActualTotalSharesValue { get; set; }
+        public List<PaymentDeadlineInfo> PaymentDeadlines { get; set; }
+    }
+}
