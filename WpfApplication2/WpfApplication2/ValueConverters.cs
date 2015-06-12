28b79cf880f8ff6bb320e557766d7d8eae4bddfe
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-12 12:31:29 +0000

    + BGU-105 WPF XAML генератор (зі структур) : further progressed

---
+++ WpfApplication2/WpfApplication2/ValueConverters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApplication2.Converters
+{
+    #region DoubleToIntegerConverter
+
+    [ValueConversion(typeof(double), typeof(int))]
+    public class DoubleToIntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)(double)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+
+    #endregion // DoubleToIntegerConverter
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((Visibility)value)
+            {
+                case Visibility.Collapsed:
+                case Visibility.Hidden:
+                    return false;
+                case Visibility.Visible:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
