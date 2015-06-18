3ab8b90f701e96e54572785488d6ccc6d89397ee
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-18 07:19:03 +0000

    + BGU-105 WPF XAML генератор (зі структур) : further progressed

--- WpfApplication2/WpfApplication2/ValueConverters.cs
+++ WpfApplication2/WpfApplication2/ValueConverters.cs
@@ -28,7 +28,7 @@ namespace WpfApplication2.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)value ? Visibility.Visible : Visibility.Hidden;
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
