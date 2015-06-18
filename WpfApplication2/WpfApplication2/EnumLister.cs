3dc774c8925162c5fceb36e58fff76fc09f39e60
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-18 19:07:21 +0000

    + BGU-105 WPF XAML генератор (зі структур) : generic template

---
+++ WpfApplication2/WpfApplication2/EnumLister.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication2
+{
+    public class EnumLister
+    {
+        //public List<object> List
+    }
+}
