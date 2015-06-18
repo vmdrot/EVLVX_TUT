91db28aeb029c83432eb4bbbc2b598df5c36d83d
jefe <vmdrot@users.noreply.github.com>
2015-06-18 19:07:21 +0000

    + BGU-105 WPF XAML генератор (зі структур) : generic template

---
+++ WpfApplication2/WpfApplication2/Forms/RegLicAppx2OwnershipAcqRequestLPFrm.xaml.cs
@@ -0,0 +1,49 @@
+﻿using BGU.DRPL.SignificantOwnership.Core.Questionnaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для RegLicAppx2OwnershipAcqRequestLPFrm.xaml
+    /// </summary>
+    public partial class RegLicAppx2OwnershipAcqRequestLPFrm : Window
+    {
+        public RegLicAppx2OwnershipAcqRequestLPFrm()
+        {
+            InitializeComponent();
+            _questionnaire = new RegLicAppx2OwnershipAcqRequestLP();
+            quCtrl.Content = Questionnaire;
+        }
+
+
+        private RegLicAppx2OwnershipAcqRequestLP _questionnaire;
+        public RegLicAppx2OwnershipAcqRequestLP Questionnaire
+        {
+            get
+            {
+                return _questionnaire;
+            }
+            set
+            {
+                _questionnaire = value;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Questionnaire.Signatory.ToString());
+        }
+    }
+}
