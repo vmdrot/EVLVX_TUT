ef46c0be7018c37572a4d59528c8307831171e58
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-22 17:48:44 +0000

    + BGU-105 WPF XAML генератор (зі структур) : generator debugged

--- WpfApplication2/WpfApplication2/Forms/RegLicAppx2OwnershipAcqRequestLPFrm.xaml.cs
+++ WpfApplication2/WpfApplication2/Forms/RegLicAppx2OwnershipAcqRequestLPFrm.xaml.cs
@@ -26,9 +26,54 @@ namespace WpfApplication2.Forms
         {
             InitializeComponent();
             _questionnaire = new RegLicAppx2OwnershipAcqRequestLP();
+            BGU.DRPL.SignificantOwnership.Utility.ReflectionUtil.InstantiateAllProps(_questionnaire, _questionnaire.GetType().Assembly);
             quCtrl.Content = Questionnaire;
             //quCtrl.Content = new BankInfo();
             //quCtrl.Content = new SignificantOwnershipAcquisitionWaysInfo();
+            //quCtrl.Content = new BankInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new BankruptcyInvestigationInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new BreachOfLawRecordInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new ContactInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new CountryInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new CourtDecisionInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new CourtInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new CreditRatingInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new CurrencyAmount(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new EconomicActivityType(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new EmailInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new FinancialOversightAuthorityInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new GenericPersonID(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new GenericPersonInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new ImperfectBusinessReputationInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new IncomeOriginInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new IndebtnessInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new IPOSharesPurchaseInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new LegalPersonInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new LegalTransactionInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new LiquidatedEntityOwnershipInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new LoanInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new LocationInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new LPRegisteredDateRecordId(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new MissingInformationResonInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new OwnershipStructure(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new OwnershipSummaryInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new OwnershipVotesInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new PaymentDeadlineInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new PersonsAssociation(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new PhoneInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new PhysicalPersonInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new PowerOfAttorneyInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new PowerOfAttorneySharesPurchaseInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new RatingAgencyInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new RegistrarAuthority(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new RegLicAppx2OwnershipAcqRequestLP(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new SecondaryMarketSharesPurchaseInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new SignatoryInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new SignificantOrDecisiveInfulenceInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new SignificantOwnershipAcquisitionWaysInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new TotalOwnershipDetailsInfo(); MessageBox.Show("Press OK to continue...");
+            //quCtrl.Content = new TotalOwnershipSummaryInfo(); MessageBox.Show("Press OK to continue...");
+
         }
 
 
@@ -47,7 +92,54 @@ namespace WpfApplication2.Forms
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(Questionnaire.Signatory.ToString());
+            //MessageBox.Show(Questionnaire.Signatory.ToString());
+            //Questionnaire.BankRef = new BankInfo();
+            //Questionnaire.Signatory = new SignatoryInfo();
+            /*
+            quCtrl.Content = new BankInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new BankruptcyInvestigationInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new BreachOfLawRecordInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new ContactInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new CountryInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new CourtDecisionInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new CourtInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new CreditRatingInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new CurrencyAmount(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new EconomicActivityType(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new EmailInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new FinancialOversightAuthorityInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new GenericPersonID(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new GenericPersonInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new ImperfectBusinessReputationInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new IncomeOriginInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new IndebtnessInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new IPOSharesPurchaseInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new LegalPersonInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new LegalTransactionInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new LiquidatedEntityOwnershipInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new LoanInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new LocationInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new LPRegisteredDateRecordId(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new MissingInformationResonInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new OwnershipStructure(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new OwnershipSummaryInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new OwnershipVotesInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new PaymentDeadlineInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new PersonsAssociation(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new PhoneInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new PhysicalPersonInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new PowerOfAttorneyInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new PowerOfAttorneySharesPurchaseInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new RatingAgencyInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new RegistrarAuthority(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new RegLicAppx2OwnershipAcqRequestLP(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new SecondaryMarketSharesPurchaseInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new SignatoryInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new SignificantOrDecisiveInfulenceInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new SignificantOwnershipAcquisitionWaysInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new TotalOwnershipDetailsInfo(); MessageBox.Show("Press OK to continue...");
+            quCtrl.Content = new TotalOwnershipSummaryInfo(); MessageBox.Show("Press OK to continue...");
+             */
         }
     }
 }
