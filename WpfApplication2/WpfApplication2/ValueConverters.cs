dab12f68a1910dd83fc51384ca186c181d8f009a
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-06-23 15:40:03 +0000

    +* BGUSO-74 WPFGen: Actionable DataGridColumns : in progress

--- WpfApplication2/WpfApplication2/ValueConverters.cs
+++ WpfApplication2/WpfApplication2/ValueConverters.cs
@@ -45,4 +45,19 @@ namespace WpfApplication2.Converters
             }
         }
     }
+
+    [ValueConversion(typeof(object[]), typeof(object))]
+    public class MultiValueConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return (object[])value;
+        }
+    }
 }
