e6e91793ef61469ed5b86d4f129a14eb3bb5f952
jefe <vmdrot@users.noreply.github.com>
2015-06-23 12:45:06 +0000

    +* BGUSO-74 WPFGen: Actionable DataGridColumns : in progress

--- WpfApplication2/WpfApplication2/Forms/RegLicAppx2OwnershipAcqRequestLPFrm.xaml.cs
+++ WpfApplication2/WpfApplication2/Forms/RegLicAppx2OwnershipAcqRequestLPFrm.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -22,9 +23,12 @@ namespace WpfApplication2.Forms
     /// </summary>
     public partial class RegLicAppx2OwnershipAcqRequestLPFrm : Window
     {
+
+        private SaveFileDialog _saveFileDgl;
         public RegLicAppx2OwnershipAcqRequestLPFrm()
         {
             InitializeComponent();
+            _saveFileDgl = new SaveFileDialog();
             _questionnaire = new RegLicAppx2OwnershipAcqRequestLP();
             BGU.DRPL.SignificantOwnership.Utility.ReflectionUtil.InstantiateAllProps(_questionnaire, _questionnaire.GetType().Assembly);
             quCtrl.Content = Questionnaire;
@@ -141,5 +145,69 @@ namespace WpfApplication2.Forms
             quCtrl.Content = new TotalOwnershipSummaryInfo(); MessageBox.Show("Press OK to continue...");
              */
         }
+
+        private string CurrentSave2File { get; set; }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DoSave();
+        }
+
+        private void SaveAsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DoSaveAs();
+        }
+        
+        private void DoSaveAs()
+        {
+            _saveFileDgl.Filter = "XML Files (.xml)|*.xml|All Files (*.*)|*.*";
+            _saveFileDgl.FilterIndex = 0;
+
+            if (!string.IsNullOrEmpty(CurrentSave2File))
+                _saveFileDgl.FileName = CurrentSave2File;
+            else if (Questionnaire != null && Questionnaire is IQuestionnaire)
+                _saveFileDgl.FileName = ((IQuestionnaire)Questionnaire).SuggestSaveAsFileName();
+
+            DialogResult userClickedOK = _saveFileDgl.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == System.Windows.Forms.DialogResult.OK)
+            {
+                // Open the selected file to read.
+                try
+                {
+                    BGU.DRPL.SignificantOwnership.Utility.Tools.WriteXML<RegLicAppx2OwnershipAcqRequestLP>(Questionnaire, _saveFileDgl.FileName);
+                    CurrentSave2File = _saveFileDgl.FileName;
+                }
+                catch (Exception exc)
+                {
+                    System.Windows.MessageBox.Show(string.Format("Failed to save file '{0}', error - '{1}'", _saveFileDgl.FileName, exc.Message), "Error");
+                }
+            }
+        }
+
+        private void DoSave()
+        {
+            if (!string.IsNullOrEmpty(CurrentSave2File))
+            {
+
+                try
+                {
+                    BGU.DRPL.SignificantOwnership.Utility.Tools.WriteXML<RegLicAppx2OwnershipAcqRequestLP>(Questionnaire, CurrentSave2File);
+                }
+                catch (Exception exc)
+                {
+                    System.Windows.MessageBox.Show(string.Format("Failed to save file '{0}', error - '{1}'", _saveFileDgl.FileName, exc.Message), "Error");
+                }
+            }
+            else
+                DoSaveAs();
+        }
+
+
     }
 }
