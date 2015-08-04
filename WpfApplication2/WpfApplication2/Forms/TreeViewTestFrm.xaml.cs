0ec368cfb593bb383f46bd790d700ca068a7effb
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-08-04 12:30:05 +0000

    +* BKINF-21 ТЗ до змін у Банк-Інфо : in progress + BKINF-24 Ієрархія відділень : in progress

---
+++ WpfApplication2/WpfApplication2/Forms/TreeViewTestFrm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для TreeViewTestFrm.xaml
+    /// </summary>
+    public partial class TreeViewTestFrm : Window
+    {
+        public TreeViewTestFrm()
+        {
+            InitializeComponent();
+        }
+    }
+}
