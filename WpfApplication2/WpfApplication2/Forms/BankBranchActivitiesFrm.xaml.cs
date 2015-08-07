957ab1ada5f608ce788d378d2dd3337dd8c9e8c0
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2015-08-07 18:56:51 +0000

    +* BKINF-21 ТЗ до змін у Банк-Інфо : in progress + bank operations/services/etc + BKINF-24 Ієрархія відділень : further refined

---
+++ WpfApplication2/WpfApplication2/Forms/BankBranchActivitiesFrm.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BGU.DRPL.SignificantOwnership.Core.Spares.Dict;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApplication2.Data;
+
+namespace WpfApplication2.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для TreeViewTestFrm.xaml
+    /// </summary>
+    public partial class BankBranchActivitiesFrm : Window
+    {
+        public BankBranchActivitiesFrm()
+        {
+            InitializeComponent();
+        }
+
+
+        protected void cbxBank_SelectionChanged(object sender, EventArgs ea)
+        {
+            //DataModule.SelectedBank = (BankInfo)cbxBank.SelectedValue;
+            //trvw.ItemsSource = DataModule.HierarchedBankDepts;
+        }
+    }
+}
