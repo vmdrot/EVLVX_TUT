21df84cfad51910f7d8a3004fba0296d5c819e11
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2016-01-14 20:17:25 +0000

    + fb2 tools

---
+++ Evolvex.Any2FbWrapper/Evolvex.Any2FbWrapper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.Any2FbWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            any_2_fb2.Any2FB2Class converter = new any_2_fb2.Any2FB2Class();
+            object rslt = converter.ConvertInteractive(0, true);
+
+        }
+    }
+}
