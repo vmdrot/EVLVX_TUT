4ba0a43a0788b8ad46291fab92b86cfd95b08094
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2016-05-11 22:42:23 +0000

    +* usr.minjust.gov.ua: tests

---
+++ VKUtil/Evolvex.VKUtilEtc.Tests/UsrMinjustReaderTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Evolvex.VKUtilLib.YeDR;
+
+namespace Evolvex.VKUtilEtc.Tests
+{
+    [TestFixture]
+    public class UsrMinjustReaderTests
+    {
+        [Test]
+        [STAThread]
+        public void ResearchTest()
+        {
+            using (UsrMinjustGovUaReader reader = new UsrMinjustGovUaReader())
+            {
+                reader.SearchForYeDRPOU = "39492190";
+                bool res = reader.Read(UsrMinjustGovUaReader.FREE_SEARCH_TRUE_URL);
+                Console.WriteLine("res = {0}", res);
+                Form1 frm = new Form1();
+                reader.ShowBrowserIn(frm.IEContainer);
+                frm.ShowDialog();
+            }
+        }
+    }
+}
