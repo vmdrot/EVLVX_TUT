a4cb62823a5b9d1c7b709727aee786cf79c60e52
jefe <vmdrot@users.noreply.github.com>
2016-05-11 22:43:42 +0000

    + Rexton reader - rough, not yet working

---
+++ VKUtil/Evolvex.VKUtilLib/Rexton/Spares/RextonFBInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.VKUtilLib.Rexton.Spares
+{
+    public class RextonFBInfo
+    {
+        public string M { get; set; }
+        public string FB { get; set; }
+    }
+}
---
+++ VKUtil/Evolvex.VKUtilLib/Rexton/Spares/RextonObjectPx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.VKUtilLib.Rexton.Spares
+{
+    public class RextonObjectPx
+    {
+        
+        public CcyAmt Single { get; set; }
+        public CcyAmt Pair { get; set; }
+        public CcyAmt Wholesale { get; set; }
+        public bool IsHalfAvailable { get; set; }
+        public CcyAmt Half { get; set; }
+    }
+}
