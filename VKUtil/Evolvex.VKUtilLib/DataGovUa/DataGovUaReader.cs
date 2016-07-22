ea533c619b5069c6e4d5a77969048bd1b26d492d
jefe <jefe@68418d72-90de-46b7-bd11-0d1125755566>
2016-07-22 17:56:44 +0000

    *+ https://github.com/CivilSocietyOnline/OpenData/issues/13 spending.gov.ua cabinet screen scraper doesn't "find" cabinets anymore #13 - further refined, seems to be fixed + ExtData: Data.gov.ua browser #25 - started;

---
+++ VKUtil/Evolvex.VKUtilLib/DataGovUa/DataGovUaReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.VKUtilLib.DataGovUa
+{
+    public class DataGovUaReader : WebBrowserReaderBase
+    {
+        protected override bool ReadWorker()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
