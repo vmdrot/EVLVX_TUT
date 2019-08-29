8731fa42ea266220304915558eb90c525cf9d4ed
vmdrot <valeriy.drotenko@gmail.com>
2019-08-29 18:30:56 +0300

    * VKUtil: tidy up #6: modified path

--- VKUtil/VKUtil/CMDs/zaycev_worker.cmd
+++ VKUtil/VKUtil/CMDs/zaycev_worker.cmd
@@ -1,4 +1,4 @@
 SET URL=%~1
 SET SAVE_DIR=%~2
 
-bin\Debug\Evolvex.VKUtil.exe ReadAndDownloadZaycevNet "%URL%" "%SAVE_DIR%" > "%SAVE_DIR%\vkutil.log" 
+..\bin\Debug\Evolvex.VKUtil.exe ReadAndDownloadZaycevNet "%URL%" "%SAVE_DIR%" > "%SAVE_DIR%\vkutil.log" 
