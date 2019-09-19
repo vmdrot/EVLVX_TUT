65fc050c91a56b5ce2e8f87faedcab08e2fab114
vmdrot <valeriy.drotenko@gmail.com>
2019-09-19 20:36:23 +0300

    + Visual Studio: *proj: detect all dependencies of an item #13: implemented

---
+++ Evolvex.VSUtils/Evolvex.VSUtils.CLI/Program.cs
@@ -0,0 +1,136 @@
+﻿using Evolvex.VSUtils.Lib;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Evolvex.VSUtils.CLI
+{
+    class Program
+    {
+        #region field(s)
+        private static Dictionary<string, CmdHandler> _cmdHandlers;
+        #endregion
+
+        #region inner type(s)
+        private delegate int CmdHandler(string[] args);
+        #endregion
+
+        #region Init CMD handlers
+        private static Dictionary<string, CmdHandler> AutoDetectCMDHandlers()
+        {
+            Dictionary<string, CmdHandler> rslt = new Dictionary<string, CmdHandler>();
+            Type thisType = typeof(Program);
+            MethodInfo[] methods = thisType.GetMethods();
+            foreach (MethodInfo mi in methods)
+            {
+                if (mi.Name == nameof(Main))
+                    continue;
+                if (mi.ReturnType != typeof(int))
+                    continue;
+                ParameterInfo[] args = mi.GetParameters();
+                if (args == null || args.Length != 1)
+                    continue;
+                if (args[0].ToString() != "System.String[] args")
+                    continue;
+                rslt.Add(mi.Name.ToLower(), (CmdHandler)mi.CreateDelegate(typeof(CmdHandler)));
+            }
+            return rslt;
+        }
+        private static void FillCmdHandlers()
+        {
+            #region auto-detect
+            _cmdHandlers = AutoDetectCMDHandlers();
+            #endregion
+
+            #region fill necessary CMD aliases (if any)
+            _cmdHandlers.Add(string.Empty, ShowUsage);
+            #endregion
+        }
+        #endregion
+
+        #region the sacred Main()
+        public static int Main(string[] args)
+        {
+            FillCmdHandlers();
+            string keyArg = args.Length > 0 ? args[0].ToLower() : null;
+            if (!string.IsNullOrWhiteSpace(keyArg) && _cmdHandlers.ContainsKey(keyArg))
+            {
+                List<string> origArgs = new List<string>(args);
+                origArgs.RemoveAt(0);
+                LogCmdArgs(_cmdHandlers[keyArg].Method.Name, origArgs.ToArray());
+                return _cmdHandlers[keyArg](origArgs.ToArray());
+            }
+            else
+                return _cmdHandlers[string.Empty](args);
+        }
+        #endregion
+
+        #region usage & help
+        public static int ShowUsage(string[] args)
+        {
+            Console.WriteLine("A valid command key must be supplied. See below the list of available commands:");
+            foreach (string key in _cmdHandlers.Keys)
+                Console.WriteLine("  {0}", key);
+            return 1;
+        }
+        #endregion
+
+        #region applied CMD handlers
+        public static int ProjListAllDependentUpon(string[] args)
+        {
+            Console.Read();
+            string projPath = args[0];
+            string depRoot = args[1];
+            bool outputAsJson = args.Length > 2 && !string.IsNullOrWhiteSpace(args[2]) ? bool.Parse(args[2]) : true;
+            List<string> deps = ProjDependenciesUtil.ListDependencies(projPath, depRoot);
+            if (outputAsJson)
+                Console.WriteLine(JsonConvert.SerializeObject(deps, Newtonsoft.Json.Formatting.Indented));
+            else
+                deps.ForEach(d => Console.WriteLine(d));
+            return 0;
+        }
+        public static int ProjCutOffItemsXmls(string[] args)
+        {
+            Console.Read();
+            string projPath = args[0];
+            string itemsListPath = args[1];
+            List<string> items = ProjDependenciesUtil.CutOffItemsXmls(projPath, File.ReadAllLines(itemsListPath));
+            items.ForEach(i => Console.WriteLine(i));
+            return 0;
+        }
+        #endregion
+
+        #region aux
+        public static int ExitWithComplaints(string msg, int ret)
+        {
+            return ExitWithComplaints(msg, ret, false);
+        }
+        public static int ExitWithComplaints(string msg, int ret, bool beep)
+        {
+            Console.WriteLine(msg);
+            if (beep)
+                Console.Beep();
+            return ret;
+        }
+
+        private static void LogCmdArgs(string methodName, string[] args)
+        {
+            StringBuilder sbArgs = new StringBuilder();
+            sbArgs.AppendFormat("routed to method {0}(", methodName);
+            if (args != null)
+            {
+                for (int i = 0; i < args.Length; i++)
+                    sbArgs.AppendLine(string.Format("[{0}]: '{1}'", i, args[i]));
+            }
+            sbArgs.AppendLine(")");
+            Console.WriteLine(sbArgs.ToString());
+        }
+        #endregion
+    }
+}
